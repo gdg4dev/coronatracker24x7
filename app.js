4da43e96dff75e5976ab4318a565148193a515cd
Aniket Hirpara <gdg4dev@gmail.com>
2020-03-18 21:10:13 +0530

    added partials

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const express = require('express')
-const app = express()
 const path = require('path')
 const hbs = require('hbs')
+const app = express()
+const handlebars = require('express-handlebars')
 const port = process.env.PORT || 3000
-const viewPath = path.join(__dirname, './templates/views')
-const partialsPath = path.join(__dirname, './templates/partials')
+const viewPath = path.join(__dirname, '/templates/views')
+const partialsPath = path.join(__dirname, '/templates/partials')
 const publicPath = path.join(__dirname, './public')
 app.use(express.static(publicPath))
 hbs.registerPartials(partialsPath)
 app.set('view engine', 'hbs')
-
-
 app.set('views', viewPath)
+
 app.get('', (req, res) => {
-    res.send('got ya')
+    res.render('index', {
+        title: true
+    })
 })
 app.get('*', (req, res) => {
     res.status(404).redirect('404.html')
 })
-
-
 app.listen(port, () => {
     console.log('Server started successfully')
 })
