ce7f8f9ae6941481ef39b12e634a2c5b3f58f3ae
Aniket Hirpara <gdg4dev@gmail.com>
2020-03-20 13:54:33 +0530

    404 solving #1

--- crons/apiv2Data.js
+++ crons/apiv2Data.js
@@ -4,11 +4,22 @@ const { Parser } = require('json2csv');
 const tableScraper = require('tabletojson')
 const url = "https://www.worldometers.info/coronavirus/";
 (() => {
-    cron.schedule('*/59 * * * *', () => {
+    cron.schedule('*/1 * * * *', () => {
         tableScraper.Tabletojson.convertUrl(url, { headings: ['countryName', 'totalCases', 'newCases', 'totalDeaths', 'newDeaths', 'totalRecovered', 'activeCases', 'seriousCases', 'casesPerM'] }, (d) => {
             const json2csvParser = new Parser();
-            const csv = json2csvParser.parse(d[0]);
-            fs.writeFileSync('./latestCoronaData/apiV2data.csv', csv)
+            const maind = json2csvParser.parse(d[0]);
+            const totalObj = d[0].filter(o => { return o.countryName == "Total:"; })[0];
+            const countryList = d[0].map((v) => {
+                return v.countryName
+            });
+            console.log(totalObj)
+            console.log(JSON.stringify(countryList))
+            console.log(d[0].map((v) => {
+                return v.countryName
+            }))
+            fs.writeFileSync('./latestCoronaData/apiV2data.csv', maind)
+            fs.writeFileSync('./latestCoronaData/totalObj.json', JSON.stringify(totalObj))
+
             console.log('apiv2data updated')
         })
 
