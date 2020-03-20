163410691a87641d69244a6047327f12bc040f14
Aniket Hirpara <gdg4dev@gmail.com>
2020-03-20 13:18:22 +0530

    crash fix #3

--- utils/coronaAPI1.js
+++ utils/coronaAPI1.js
@@ -1,6 +1,6 @@
 const csv = require('csvtojson')
 const path = require('path')
-let newFilePath = path.join(__dirname, '../latestCoronaData/apiV1data.csv')
+let newFilePath = path.join(__dirname, '../latestCoronaData/apiV1Data.csv')
 const convertDataIntoJsonAPI = (d) => {
     csv()
         .fromFile(newFilePath)
