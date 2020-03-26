068ebbed8c89042f8aa2764a5dc8c8977189d162
Aniket Hirpara <gdg4dev@gmail.com>
2020-03-27 00:44:18 +0530

    Added Graphs and Added all scrapers as cron for 15 minutes

--- test.js
+++
@@ -1,7 +0,0 @@
-import covidFetcher from 'simple-covid19-json-fetcher'
-(async() => {
-
-    // Attempt to fetch today's data - fallbacks to previous day on 404
-    const covidCountries = await corona(new Date())
-    console.log(covidCountries)
-})()
